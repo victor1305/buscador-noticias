9c48271e382c7dcafdec5e8ae9ddc112ba78fd06
Victor Sanchez <victor1305@icloud.com>
2020-09-29 17:52:31 +0200

    [STABLE][v1.0]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoNoticias from "./components/ListadoNoticias"
+
 
 function App() {
+
+  //Definir la categoría
+
+  const [ categoria, guardarCategoria ] = useState('')
+  const [ noticias, guardarNoticias ] = useState([]) 
+
+  useEffect(() => {
+
+    const consultarApi = async() => {
+
+      const url = `http://newsapi.org/v2/top-headlines?country=gb&category=${categoria}&apiKey=c36b6ae056b54b3a805904e589d114bf`
+
+      const respuesta = await fetch(url)
+      const noticias = await respuesta.json()
+
+      guardarNoticias(noticias.articles)
+
+    }
+    consultarApi()
+  }, [categoria])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header 
+        titulo = "Buscardor de Noticias"
+      />
+      <div className = "container white">
+        <Formulario 
+          guardarCategoria = {guardarCategoria}
+        />
+        <ListadoNoticias
+          noticias = {noticias}
+        />
+      </div>
+    </Fragment>
   );
 }
 
